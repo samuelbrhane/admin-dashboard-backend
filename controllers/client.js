830d3c25d4a75e8307407ace0cbc27aa5ecd4b86
samuelberhane <1samibrhane@gmail.com>
2023-03-07 16:00:01 +0300

    get all customers route.

--- controllers/client.js
+++ controllers/client.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product");
 const ProductStat = require("../models/productStat");
+const User = require("../models/user");
 
 // get all products
 const getProducts = async (req, res) => {
@@ -18,4 +19,14 @@ const getProducts = async (req, res) => {
   }
 };
 
-module.exports = { getProducts };
+// get all customers
+const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { getProducts, getCustomers };
--- routes/client.js
+++ routes/client.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts } = require("../controllers/client");
+const { getProducts, getCustomers } = require("../controllers/client");
 
+// get all products
 router.get("/products", getProducts);
+
+// get all customers
+router.get("/customers", getCustomers);
 
 module.exports = router;
